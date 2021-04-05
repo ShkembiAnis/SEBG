33c6918bcdad37f28a6dc0765708ecb52518eee7
Anis <if19b505@technikum-wien.at>
2021-04-05 21:15:56 +0200

    Database created and connection set

--- src/main/java/server/PostGre.java
+++ src/main/java/server/PostGre.java
@@ -1,4 +1,18 @@
 package server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostGre {
+    Connection connection;
+    public PostGre(){
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/SBEG",
+                    "postgres", "hazard<3");
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
 }
